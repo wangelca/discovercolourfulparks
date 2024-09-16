2bab0c2b639f5d573b531406da20cb31e704c204
wuiyit <wuiyi.tang@edu.sait.ca>
2024-09-16 15:21:33 -0600

    create header components

--- app/parks/page.js
+++ app/parks/page.js
@@ -2,6 +2,7 @@
 
 import { useUser, SignInButton, SignOutButton } from '@clerk/nextjs';
 import { useState } from 'react';
+import Header from '../components/header';
 
 export default function Home() {
   const { isSignedIn } = useUser(); // Check if the user is signed in
@@ -26,37 +27,7 @@ export default function Home() {
 
   return (
     <div className="relative flex flex-col min-h-screen bg-gray-100">
-      <header className="bg-white shadow-lg">
-        <div className="container mx-auto flex justify-between items-center px-6 py-4">
-          <div className="text-3xl font-extrabold text-gray-800">
-            Discover Colourful Parks
-          </div>
-          
-          <nav className="hidden md:flex flex-grow justify-center space-x-8">
-            <a href="/parks" className="text-gray-700 hover:text-gray-900 transition">Parks</a>
-            <a href="/events" className="text-gray-700 hover:text-gray-900 transition">Events</a>
-            <a href="/" className="text-gray-700 hover:text-gray-900 transition">Spots</a>
-            <a href="/" className="text-gray-700 hover:text-gray-900 transition">Fees</a>
-            <a href="/" className="text-gray-700 hover:text-gray-900 transition">About Us</a>
-          </nav>
-
-          <div>
-            {!isSignedIn ? (
-              <SignInButton redirectUrl="/login">
-                <button className="bg-black text-white font-semibold py-2 px-4 rounded-full transition hover:bg-gray-700">
-                  Sign In
-                </button>
-              </SignInButton>
-            ) : (
-              <SignOutButton>
-                <button className="bg-black text-white font-semibold py-2 px-4 rounded-full transition hover:bg-gray-700">
-                  Sign Out
-                </button>
-              </SignOutButton>
-            )}
-          </div>
-        </div>
-      </header>
+      <Header/>
 
       <main className="container mx-auto px-6 py-12 flex flex-col">
         <h1 className="text-3xl font-bold text-center mb-8 text-gray-800">Find a Park</h1>
