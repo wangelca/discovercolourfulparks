b2aa5ea1dd37fdc455f512898e18805fe18e20b3
wuiyit <wuiyi.tang@edu.sait.ca>
2024-09-18 00:27:51 -0600

    add api - sync the clerk database with postgre (you may run "npx prisma studio" to validate if the data has been sync)

--- middleware.js
+++ middleware.js
@@ -3,21 +3,25 @@ import { NextResponse } from "next/server";
 
 export default authMiddleware({
   // Define public routes that should not require authentication
-  publicRoutes: ["/", "/parks", "/events", "/spots", "/fees", "/about", "/explore", "/api/user"], 
-// Remove the extra closing curly brace
-// });
+  publicRoutes: ["/", "/parks", "/events", "/spots", "/fees", "/about", "/explore", "/api/user", "/api/clerkSync"], 
 
   // Ignore Clerk authentication on static assets and API routes
   ignoredRoutes: [
-    "/((?!api|trpc))(_next.*|.+\\.[\\w]+$)", // Exclude next.js static files and asset routes
-    "/"
+    "/((?!api|trpc))(_next.*|.+\\.[\\w]+$)", "/"// Exclude next.js static files and asset routes
   ],
     
       // Custom afterAuth function to modify Clerk's default redirect behavior
   afterAuth(auth, req) {
+    const { pathname } = req.nextUrl;
+
+    // Make sure the /api/clerkSync route is excluded from the auth checks
+    if (pathname.startsWith('/api/clerkSync')) {
+      return NextResponse.next(); // Allow access to /api/clerkSync
+    }
+
     // Allow both signed-in and signed-out users to access public routes
     if (!auth.isPublic && !auth.userId) {
-      return NextResponse.redirect(new URL("/sign-in", req.url)); // Redirect to sign-in if not authenticated
+      return NextResponse.redirect(new URL("/", req.url)); // Redirect to homepage if not authenticated
     }
     return NextResponse.next(); // Proceed to next middleware or route
   },
