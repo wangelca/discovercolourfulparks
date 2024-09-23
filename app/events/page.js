5b6c221b89adcc4db0cf23c05144d60bf7fdda42
wuiyit <wuiyi.tang@edu.sait.ca>
2024-09-23 17:51:34 -0600

    Move all listings API caller to component, and ensure the database (with data) are connected

--- app/events/page.js
+++ app/events/page.js
@@ -1,12 +1,13 @@
 'use client';
 
 import Header from "../components/header";
+import Events from "../components/eventlist";
 
 export default function Home() {
   return (
     <div className="relative flex flex-col min-h-screen bg-gray-100">
             <Header/>
-            Event list to be shown
+            <Events/>
     </div>
 
   );
