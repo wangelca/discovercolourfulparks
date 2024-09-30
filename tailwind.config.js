b3160cd6b5a021e0ce0bdfa3ef3bee5cd3deccfd
wuiyit <wuiyi.tang@edu.sait.ca>
2024-09-30 13:00:44 -0600

    CSS design - update logo, update responsive header, add footer component

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,9 @@ module.exports = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        'dosis': ["Dosis", "sans-serif"],
+      },
     },
   },
   plugins: [],
