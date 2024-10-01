af3f1b922769212e04abda161d294746fd2d54f6
wuiyit <wuiyi.tang@edu.sait.ca>
2024-09-30 18:30:30 -0600

    Park list - update from hardcoded page and update schema, now fetch the data from database based on province

--- pages/api/park.js
+++ pages/api/park.js
@@ -1,16 +1,18 @@
-import { prisma } from '../../app/lib/prisma';
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
-  if (req.method === 'GET') {
-    try {
-      const parks = await prisma.park.findMany(); // Fetch all parks
-      res.status(200).json(parks);
-    } catch (error) {
-      console.error('Error fetching parks:', error);
-      res.status(500).json({ error: 'Unable to fetch parks' });
-    }
-  } else {
-    res.setHeader('Allow', ['GET']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
+  const { province } = req.query;
+
+  try {
+    // Fetch parks filtered by province if province query param is present
+    const parks = await prisma.park.findMany({
+      where: province ? { province: decodeURIComponent(province) } : {},
+    });
+
+    res.status(200).json(parks);
+  } catch (error) {
+    res.status(500).json({ error: "Error fetching parks" });
   }
 }
