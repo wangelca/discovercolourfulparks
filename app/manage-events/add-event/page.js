83dd716ff2c51908c5a6a823cd19de0283d2cbe5
wuiyit <wuiyi.tang@edu.sait.ca>
2024-10-09 21:45:06 -0600

    Add "Add Event" page

--- app/manage-events/add-event/page.js
+++ app/manage-events/add-event/page.js
@@ -3,16 +3,21 @@
 import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import axios from "axios";
+import ImageUploadComponent from "../../components/image-upload.js";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const AddEventPage = () => {
   const [formData, setFormData] = useState({
     parkId: "",
-    spotName: "",
-    spotDescription: "",
-    spotHourlyRate: "",
-    spotDiscount: "",
-    spotLocation: "",
-    spotImageUrl: null,
+    eventName: "",    
+    eventLocation: "",
+    description: "",
+    fee: "",
+    discount: "",
+    eventImageUrl: null,
+    startDate: "",
+    endDate: "",
     requiredbooking: false,
   });
   const [errors, setErrors] = useState({});
@@ -35,12 +40,12 @@ const AddEventPage = () => {
 
   const handleInputChange = (e) => {
     const { name, value, type, checked } = e.target;
-    if (name === "spotHourlyRate" || name === "spotDiscount") {
+    if (name === "fee" || name === "discount") {
       if (value < 0 || value > 9999) {
         setErrors({
           ...errors,
           [name]: `${
-            name === "spotHourlyRate" ? "Hourly rate" : "Discount"
+            name === "fee" ? "Fee" : "Discount"
           } must be between 0 and 9999`,
         });
       } else {
@@ -56,128 +61,110 @@ const AddEventPage = () => {
     });
   };
 
-  const handleFileChange = (e) => {
-    const file = e.target.files[0];
-    const allowedFormats = ["image/jpeg", "image/png"];
-    const maxSize = 10 * 1024 * 1024;
+  const handleDateChange = (date, fieldName) => {
+    setFormData({ ...formData, [fieldName]: date });
+  };
 
-    if (!allowedFormats.includes(file.type)) {
-      setErrors({
-        ...errors,
-        spotImages: "Only JPEG and PNG formats are allowed",
-      });
-      return;
+  const handleFileChange = (file, error) => {
+    if (error) {
+      setErrors({ ...errors, eventImageUrl: error });
+    } else {
+      setFormData({ ...formData, eventImageUrl: file });
+      setErrors({ ...errors, eventImageUrl: null });
     }
-
-    if (file.size > maxSize) {
-      setErrors({ ...errors, spotImages: "File size should not exceed 10MB" });
-      return;
-    }
-
-    // Validate image resolution
-    const reader = new FileReader();
-    reader.onload = (event) => {
-      const img = new Image();
-      img.onload = () => {
-        if (img.width < 800 || img.height < 600) {
-          setErrors({
-            ...errors,
-            spotImages: "Image resolution should be at least 800x600 pixels",
-          });
-          return;
-        }
-        // If validation passes
-        setFormData({ ...formData, spotImages: [file] });  // Array with one image
-        setErrors({ ...errors, spotImages: null }); // Clear errors
-      };
-      img.src = event.target.result;
-    };
-    reader.readAsDataURL(file);
   };
 
   const validateInput = () => {
     const newErrors = {};
     if (!formData.parkId) newErrors.parkId = "Park ID is required";
-    if (!formData.spotName) newErrors.spotName = "Spot name is required";
-    if (!formData.spotDescription)
-      newErrors.spotDescription = "Description is required";
+    if (!formData.eventName) newErrors.eventName = "Event name is required";
+    if (!formData.description)
+      newErrors.description = "Description is required";
     if (
-      !formData.spotHourlyRate ||
-      isNaN(formData.spotHourlyRate) ||
-      formData.spotHourlyRate < 0 ||
-      formData.spotHourlyRate > 9999
+      !formData.fee ||
+      isNaN(formData.fee) ||
+      formData.fee < 0 ||
+      formData.fee > 9999
     ) {
-      newErrors.spotHourlyRate = "Hourly rate must be between 0 and 9999";
+      newErrors.fee = "Fee must be between 0 and 9999";
     }
     if (
-      !formData.spotDiscount ||
-      isNaN(formData.spotDiscount) ||
-      formData.spotDiscount < 0 ||
-      formData.spotDiscount > 9999
+      !formData.discount ||
+      isNaN(formData.discount) ||
+      formData.discount < 0 ||
+      formData.discount > 9999 ||
+      formData.discount > formData.fee
     ) {
-      newErrors.spotDiscount = "Discount must be between 0 and 9999";
+      newErrors.discount = "Discount must be between 0 and 9999, and less than fee";
     }
-    if (!formData.spotLocation) newErrors.spotLocation = "Location is required";
-    if (!formData.spotImages) newErrors.spotImages = "Spot image is required";
-    setErrors(newErrors);
-    return Object.keys(newErrors).length === 0;
+    if (!formData.startDate) newErrors.startDate = "Start date is required";
+    if (!formData.endDate) newErrors.endDate = "End date is required";
+    if (!formData.eventLocation) newErrors.eventLocation = "Location is required";
+    if (formData.startDate > formData.endDate) newErrors.startDate = "Start date must be before end date";
+    if (!formData.eventImageUrl)
+      newErrors.eventImageUrl = "Event image is required";
+    setErrors(newErrors); 
+    console.log("Validation errors:", newErrors);  // Log the errors to see if any exist
+     return Object.keys(newErrors).length === 0;
   };
 
   const handleReview = () => {
     if (validateInput()) {
       setSummary(formData);
+      console.log("Summary Data:", formData);  // Log to see what data is being set
     }
   };
 
   const handleSubmit = async () => {
+
     const formDataObj = new FormData();
     formDataObj.append("parkId", formData.parkId);
-    formDataObj.append("spotName", formData.spotName);
-    formDataObj.append("spotDescription", formData.spotDescription);
-    formDataObj.append("spotHourlyRate", formData.spotHourlyRate);
-    formDataObj.append("spotDiscount", formData.spotDiscount);
-    formDataObj.append("spotLocation", formData.spotLocation);
+    formDataObj.append("eventName", formData.eventName);
+    formDataObj.append("description", formData.description);
+    formDataObj.append("fee", formData.fee);
+    formDataObj.append("discount", formData.discount);
+    formDataObj.append("eventLocation", formData.eventLocation);
     formDataObj.append("requiredbooking", formData.requiredbooking);
-    if (formData.spotImages && formData.spotImages.length === 1) {
-      formDataObj.append("spotImageUrl", formData.spotImages[0]);  // Append one image
-    }
+    formDataObj.append("startDate", formData.startDate.toISOString().slice(0, 19));
+    formDataObj.append("endDate", formData.endDate.toISOString().slice(0, 19));
+    if (formData.eventImageUrl) {
+      formDataObj.append("eventImageUrl", formData.eventImageUrl);  // Append the uploaded image
+  }
 
     try {
-      await axios.post("http://localhost:8000/spots/add", formDataObj, {
+      await axios.post("http://localhost:8000/events/add", formDataObj, {
         headers: { "Content-Type": "multipart/form-data" },
       });
-      router.push("/manage-spots"); // Redirect to the spots list page after success
+      router.push("/manage-events"); 
     } catch (error) {
-      console.error("Failed to add spot", error);
+      console.error("Failed to add event", error);
     }
   };
-
 
   return (
     <div className="container mx-auto p-6">
       {!summary ? (
         <form className="max-w-lg mx-auto">
           <h2 class="mb-4 text-xl font-bold text-gray-900 dark:text-white">
-            Add a spot
+            Add an Event
           </h2>
           <div class="grid gap-4 mb-4 sm:grid-cols-2 sm:gap-6 sm:mb-5">
             <div class="sm:col-span-2">
               <label
                 for="name"
-                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-              >
-                Spot Name
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">      
+                Event Name
               </label>
               <input
                 type="text"
-                name="spotName"
-                value={formData.spotName}
+                name="eventName"
+                value={formData.eventName}
                 onChange={handleInputChange}
                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                placeholder="Type spot name"
+                placeholder="Type event name"
               />
-              {errors.spotName && (
-                <span className="text-red-500">{errors.spotName}</span>
+              {errors.eventName && (
+                <span className="text-red-500">{errors.eventName}</span>
               )}
             </div>
 
@@ -211,39 +198,39 @@ const AddEventPage = () => {
                 for="description"
                 className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
               >
-                Spot Description
+                Event Description
               </label>
               <textarea
                 id="description"
                 rows="8"
-                name="spotDescription"
-                value={formData.spotDescription}
+                name="description"
+                value={formData.description}
                 onChange={handleInputChange}
                 className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                 placeholder="Write a description here"
               />
-              {errors.spotDescription && (
-                <span className="text-red-500">{errors.spotDescription}</span>
+              {errors.description && (
+                <span className="text-red-500">{errors.description}</span>
               )}
             </div>
 
             <div class="w-full">
               <label
-                for="hourlyRate"
+                for="fee"
                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
               >
-                Hourly Rate (in decimals)
+                Fee (in decimals)
               </label>
               <input
                 type="number"
-                name="spotHourlyRate"
-                value={formData.spotHourlyRate}
+                name="fee"
+                value={formData.fee}
                 onChange={handleInputChange}
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                 placeholder="50"
               />
-              {errors.spotHourlyRate && (
-                <span className="text-red-500">{errors.spotHourlyRate}</span>
+              {errors.fee && (
+                <span className="text-red-500">{errors.fee}</span>
               )}
             </div>
 
@@ -256,52 +243,76 @@ const AddEventPage = () => {
               </label>
               <input
                 type="number"
-                name="spotDiscount"
-                value={formData.spotDiscount}
+                name="discount"
+                value={formData.discount}
                 onChange={handleInputChange}
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                 placeholder="0"
               />
-              {errors.spotDiscount && (
-                <span className="text-red-500">{errors.spotDiscount}</span>
+              {errors.discount && (
+                <span className="text-red-500">{errors.discount}</span>
+              )}
+            </div>
+            <div class="w-full">
+              <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                Start Date & Time
+              </label>
+              <DatePicker
+              showIcon
+              selected={formData.startDate}
+              onChange={(date) => handleDateChange(date, "startDate")}
+              showTimeSelect
+              dateFormat="Pp"              
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"
+            />
+              {errors.startDate && (
+                <span className="text-red-500">{errors.startDate}</span>
+              )}
+            </div>
+            <div class="w-full">
+              <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                End Date & Time
+              </label>
+              <DatePicker
+              showIcon
+              selected={formData.endDate}
+              onChange={(date) => handleDateChange(date, "endDate")}
+              showTimeSelect
+              dateFormat="Pp"              
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"
+            />
+              {errors.endDate && (
+                <span className="text-red-500">{errors.endDate}</span>
               )}
             </div>
 
             <div class="w-full">
               <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                Spot Location
+                Event Location
               </label>
               <input
                 type="text"
-                name="spotLocation"
-                value={formData.spotLocation}
+                name="eventLocation"
+                value={formData.eventLocation}
                 onChange={handleInputChange}
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
               />
-              {errors.spotLocation && (
-                <span className="text-red-500">{errors.spotLocation}</span>
+              {errors.eventLocation && (
+                <span className="text-red-500">{errors.eventLocation}</span>
               )}
             </div>
             <div>
-              <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                Spot Image
-              </label>
-              <input
-                type="file"
-                name="spotImage"
-                onChange={handleFileChange}
-                className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+              <ImageUploadComponent
+                onFileChange={handleFileChange}
+                errors={errors.eventImages}
               />
-              {errors.spotImageUrl && (
-                <span className="text-red-500">{errors.spotImageUrl}</span>
-              )}
             </div>
 
             <div>
               <label className="inline-flex items-center mb-5 cursor-pointer">
                 <input
                   type="checkbox"
-                  name="requiredBooking"
+                  name="requiredbooking"
                   checked={formData.requiredbooking}
                   onChange={handleInputChange} // Keep your change handler
                   className="sr-only peer"
@@ -327,64 +338,7 @@ const AddEventPage = () => {
       ) : (
         <div className="container mx-auto p-6">
           <h1>Summary</h1>
-          {/* Spot Content */}
-          <div className="p-6">
-            <h2 className="text-4xl font-bold text-gray-800 mb-4">
-              {summary.spotName}
-            </h2>
-            {/* Spot Image */}
-            {summary.spotImageUrl && (
-              <img
-                src={URL.createObjectURL(summary.spotImageUrl)}
-                alt={`Image of ${summary.spotName}`}
-                className="w-full h-64 object-cover"
-              />
-            )}
-            <div className="flex flex-col space-y-4">
-              <p className="text-lg">
-                <strong className="font-semibold">Park ID:</strong>{" "}
-                {summary.parkId}
-              </p>
-              <p className="text-lg">
-                <strong className="font-semibold">Location:</strong>{" "}
-                {summary.spotLocation}
-              </p>
-              <p className="text-lg">
-                <strong className="font-semibold">Description:</strong>{" "}
-                {summary.spotDescription}
-              </p>
-              <p className="text-lg">
-                <strong className="font-semibold">Hourly Rate:</strong>
-                <span className="text-green-600 font-bold">
-                  {" "}
-                  ${summary.spotHourlyRate}
-                </span>
-              </p>
-              <p className="text-lg">
-                <strong className="font-semibold">Discount:</strong>
-                <span className="text-red-600 font-bold">
-                  {" "}
-                  {summary.spotDiscount}%
-                </span>
-              </p>
-              <p>
-                Required Booking: {summary.requiredbooking ? "True" : "False"}
-              </p>
-            </div>
-            {/* Google Maps Embed */}
-            <div className="mt-6">
-              <iframe
-                width="100%"
-                height="300"
-                frameBorder="0"
-                style={{ border: 0, borderRadius: "12px" }}
-                referrerPolicy="no-referrer-when-downgrade"
-                src={`https://www.google.com/maps/embed/v1/place?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&q=${summary.parameters}`}
-                allowFullScreen
-              ></iframe>
-            </div>
-          </div>
-
+          To update the review layout
           <button
             onClick={handleSubmit}
             className="btn bg-green-500 text-white"
@@ -395,6 +349,6 @@ const AddEventPage = () => {
       )}
     </div>
   );
-}
+};
 
-export default AddSpotPage;
+export default AddEventPage;
