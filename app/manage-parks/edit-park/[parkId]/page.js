a71a26617b4a37de323e12dc64482688d28dc168
Angelica Pekas <verzangelica@gmail.com>
2024-10-18 15:21:43 -0600

    references

--- app/manage-parks/edit-park/[parkId]/page.js
+++ app/manage-parks/edit-park/[parkId]/page.js
@@ -171,6 +171,7 @@ export default function EditParkPage() {
 Chatgpt prompt: For line24, I'm being redirected instead to http://localhost:3000/manage-parks/edit-park/undefined. 
                 My routing was fine before, how do I fix this and what do I change this into so it loads into the proper area?
 
+axios routing: Semi copied from Wui's add branch page, applying the logic she used add parks and the routing to our API
 
 */
 
